06c7221413a11b157c9e7b39a0fe16019c7ada99
tjeandot <teofilo.jeandot@gmail.com>
2020-03-05 11:46:35 +0100

    Revert "create quote controller/dto/interface/module/service"
    This reverts commit 15d1de3d4cd04a654de3ab9eb6043d7331c67e69.

--- backend/src/quote/quote.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Quote extends Document {
-    readonly quote: String,
-    readonly  actor: String,
-    readonly author: String,
-    readonly season: String,
-    readonly episode: String,
- }
--- backend/src/quote/quote.schema.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-export const QuoteSchema = new mongoose.Schema({
-    quote: String,
-    actor: String,
-    author: String,
-    season: String,
-    episode: String,
-
-})
