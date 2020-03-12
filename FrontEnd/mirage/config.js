77cdf2c25bb07e9dcc1fbb10dbed43bad51e0bb9
MAEL MAEL <mm6195@hotmail.fr>
2020-03-12 11:30:43 +0100

    add leaderboard bug 304 modified

--- FrontEnd/mirage/config.js
+++ FrontEnd/mirage/config.js
@@ -3,5 +3,8 @@ export default function() {
   this.get('/quotes', (schema, request) => {
     return schema.quotes.all();
   });
+  this.get('/leaderboards/topTen'),(schema, request) =>{
+    return schema.leaderboards.all()
+  }
 
 }
